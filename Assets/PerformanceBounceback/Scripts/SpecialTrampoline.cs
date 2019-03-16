506d891cb4e96926c04fcd980d431d40eb828ea6
LegoBB8 <stevenng2005@gmail.com>
2019-03-16 23:04:53 +0800

    Original Copy

---
+++ Assets/PerformanceBounceback/Scripts/SpecialTrampoline.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialTrampoline : MonoBehaviour
+{
+
+    public ParticleSystem pSystem;
+    public GameManager scoreScript;
+
+    // Use this for initialization
+    void Start()
+    {
+        pSystem = GetComponentInChildren<ParticleSystem>();
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Throwable"))
+        {
+            //Score Point
+            scoreScript.score = scoreScript.score + 2;
+            //Particle effect
+            pSystem.Play();
+
+
+        }
+
+    }
+}
